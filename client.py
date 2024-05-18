70e04a4db9dadec5433b3be619d9e7857c02f7af
Donna Kaytlyn <trinhhtk.5903@gmail.com>
2024-05-19 03:20:47 +0700

    commit

--- client.py
+++ client.py
@@ -1,50 +1,41 @@
+#client
 import socket
 import pickle
 import psutil
+import time
 import platform
 
-def get_client_info():
-    # Lấy thông tin về người dùng từ psutil
-    users = psutil.users()
-    user_info = []
-    for user in users:
-        username = user.name
-        terminal = user.terminal
-        host = user.host
-        started = user.started
-        user_info.append({
-            'username': username,
-            'terminal': terminal,
-            'host': host,
-            'started': started
-        })
-    print(user_info)
+def get_running_processes():
+    running_processes = []
+    for process in psutil.process_iter(['pid', 'ppid', 'name', 'username', 'status']):
+        # Bỏ qua các tiến trình hệ thống và các tiến trình không có tên người dùng
+        if process.info['username'] is not None and process.info['username'] != 'SYSTEM':
+            running_processes.append({
+                'pid': process.info['pid'],
+                'ppid': process.info['ppid'],
+                'name': process.info['name'],
+                'username': process.info['username'],
+                'status': process.info['status'],
+                'os' : platform.system(),
+                'version' : platform.release(),
+            })
+            
+    return running_processes
 
-    # Lấy thông tin về hệ điều hành của client
-    os_info = {
-        'os': platform.system(),  # Hệ điều hành của client
-        'version': platform.version()  # Phiên bản của hệ điều hành
-    }
-    print(os_info)
-    # Kết hợp thông tin về người dùng và thông tin về hệ điều hành
-    client_info = {
-        'user_info': user_info,
-        'os_info': os_info
-    }
-    return client_info
-
-def send_client_info(host, port, client_info):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect((host, port))
-            data = pickle.dumps(client_info)
-            s.sendall(data)
-            print("Client info sent to server")
-        except Exception as e:
-            print(f"Error: {e}")
+def send_running_processes(host, port):
+    while True:  # Vòng lặp vô hạn để gửi thông tin định kỳ
+        processes = get_running_processes()
+        data = pickle.dumps(processes)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((host, port))
+                s.sendall(data)
+                print("Sent running processes to server")
+            except Exception as e:
+                print(f"Error: {e}")
+        time.sleep(0)  # Đợi 5 giây trước khi gửi lại
 
 if __name__ == "__main__":
-    SERVER_HOST = '192.168.1.100'  # Thay bằng địa chỉ IP của server
+    SERVER_HOST = '192.168.60.130'
     SERVER_PORT = 12345
-    client_info = get_client_info()
-    send_client_info(SERVER_HOST, SERVER_PORT, client_info)
+    send_running_processes(SERVER_HOST, SERVER_PORT)
